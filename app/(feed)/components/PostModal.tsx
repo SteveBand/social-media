8eb5d786ba7234bc0f26a10eb97263eb22839d3e
Steve Bandarker <steve020629@icloud.com>
2023-11-25 19:56:58 +0200

    post modal design changed

--- app/(feed)/components/PostModal.tsx
+++ app/(feed)/components/PostModal.tsx
@@ -7,41 +7,25 @@ import { CiImageOn } from "react-icons/ci";
 type Props = {};
 
 export default function PostModal({}: Props) {
-  const [height, setHeight] = useState(20);
-  function handleTextArea(e: React.ChangeEvent<HTMLTextAreaElement>) {
-    const scrollHeight = e.target.scrollHeight;
-    const defaultScrollHeight = 22;
-    if (scrollHeight === 22) {
-      return;
-    }
-    if (scrollHeight > defaultScrollHeight) {
-      const newHeight = scrollHeight - defaultScrollHeight;
-      setHeight(newHeight);
-    }
-  }
+  const [height, setHeight] = useState(34);
+
   return (
     <section className="feed-post-modal-wrapper">
       <form className="feed-post-modal-container">
-        <article className="feed-post-modal-upper">
-          <div className="feed-post-modal-profile-image">
-            <PiUserCircle className="big-image" />
-            <div className="line"></div>
-            <PiUserCircle className="small-image" />
-          </div>
-          <div className="content">
-            <div className="content-title">
-              <h6>steve.bndkr</h6>
-              <textarea
-                id="description"
-                placeholder="Start a post..."
-                style={{ height: `${height}px` }}
-                onChange={handleTextArea}
-              />
-            </div>
-            <CiImageOn className="upload-image-icon" />
-            <p className="add-to-post-button">Add to post</p>
-          </div>
-        </article>
+        <div className="profile-content">
+          <PiUserCircle className="icon" />
+          <p>Steve.Bndkr</p>
+        </div>
+        <div className="inputs">
+          <span
+            className="textarea"
+            role="textbox"
+            contentEditable
+            placeholder="Write here"
+          ></span>
+          <CiImageOn className="icon" />
+        </div>
+        <button className="feed-post-modal-button">Post</button>
       </form>
     </section>
   );
