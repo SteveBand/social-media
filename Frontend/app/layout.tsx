387a510b84f612d3d52c5689ecf7a6dec6a8fdf7
Steve Bandarker <steve020629@icloud.com>
2024-01-07 13:52:40 +0200

    profile Picture modal added

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -1,4 +1,3 @@
-import Navbar from "@/components/navbar/Navbar";
 import AuthProvider from "@/lib/AuthProvider";
 import { ReduxProvider } from "@/redux/Provider";
 import "@/styles/main.scss";
--- Frontend/components/ProfileImage.tsx
+++ Frontend/components/ProfileImage.tsx
@@ -1,13 +1,26 @@
 import { User } from "../../types";
-
+import { useRouter } from "next/navigation";
 type Props = {
   userInfo: User;
 };
 
 export function ProfileImage({ userInfo }: Props) {
+  const router = useRouter();
   return (
     <>
       <img src={userInfo.avatar_url} className="post-profile-icon" />
+      <div
+        className="profile-pop-modal"
+        onClick={() => router.push(`/profile/${userInfo.email}`)}
+      >
+        <img src={userInfo.avatar_url} />
+        <h5>{userInfo.name}</h5>
+        <p>{userInfo.bio}</p>
+        <div className="followers">
+          <p>{`${userInfo.followers} Followers`}</p>
+          <p>{`${userInfo.following} Following`}</p>
+        </div>
+      </div>
     </>
   );
 }
