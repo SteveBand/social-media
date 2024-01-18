94944351218a3bef37dafad37d1e83a456fd61c9
Steve Bandarker <steve020629@icloud.com>
2024-01-18 19:02:53 +0200

    CommunityLink component

--- Frontend/app/communities/page.tsx
+++ Frontend/app/communities/page.tsx
@@ -1,14 +1,38 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CommunitiesPageHeader } from "./components/CommunitiesHeader";
+import { CommunityType } from "../../../types";
+import Link from "next/link";
+import { CommunityLink } from "./components/CommunityLink";
 
 export default function CommunitiesPage() {
-  const [list, setList] = useState();
+  const [communitieslist, setCommunitiesList] = useState<CommunityType[]>([]);
+
+  async function fetchCommunities() {
+    const res = await fetch("http://localhost:4000/communities", {
+      method: "GET",
+      credentials: "include",
+    });
+    if (res.ok) {
+      const data = await res.json();
+      setCommunitiesList(data);
+    }
+  }
+
+  useEffect(() => {
+    if (communitieslist.length <= 0) {
+      fetchCommunities();
+    }
+  }, []);
   return (
     <section className="communities-page-container">
       <CommunitiesPageHeader />
-      <section className="communities-list"></section>
+      <section className="communities-list">
+        {communitieslist.map((community) => {
+          return <CommunityLink community={community} />;
+        })}
+      </section>
     </section>
   );
 }
